a810e84640b3ff16070764414f49bea192d44235
Gocha64 <70147992+Gocha64@users.noreply.github.com>
2023-03-29 17:38:55 +0900

    feat: 홈 컨트롤러 작성

---
+++ jjamppong/src/main/java/gocha/jjamppong/controller/HomeController.java
@@ -0,0 +1,17 @@
+package gocha.jjamppong.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class HomeController {
+
+    @GetMapping("/")
+    public String index(){
+
+        return "index";
+    }
+
+}
